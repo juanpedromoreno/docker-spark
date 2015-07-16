bb0c41682e9a040a8f0cfd02793f2a51c1b2de36
Juan Pedro Moreno <juanpmorest@gmail.com>
2015-07-16 21:30:07 +0200

    Upgrades spark to 1.4.1 version

--- start-master.sh
+++ start-master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker pull epahomov/docker-spark
-docker run -d -t -P --name spark_master epahomov/docker-spark /start-master.sh "$@"
+docker pull juanpedromoreno/docker-spark
+docker run -d -t -p 7077:7077 -p 8080:8080 -P --name spark_master -v /Users/juanpedromoreno/ab/:/opt/conf -v /Users/juanpedromoreno/workspace/47deg/aurora-breeze/trunk/checkpoint:/usr/local/spark/checkpoint juanpedromoreno/docker-spark /start-master.sh "$@"
