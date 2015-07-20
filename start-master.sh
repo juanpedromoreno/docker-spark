2fc3acf650e7475031617787352f2faf606fb626
Juan Pedro Moreno <juanpmorest@gmail.com>
2015-07-20 17:40:11 +0200

    Configures start-master and start-worker scripts to use the scala 2.11 spark image

--- start-master.sh
+++ start-master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker pull juanpedromoreno/docker-spark
-docker run -d -t -p 7077:7077 -p 8080:8080 -P --name spark_master -v /Users/juanpedromoreno/ab/:/opt/conf -v /Users/juanpedromoreno/workspace/47deg/aurora-breeze/trunk/checkpoint:/usr/local/spark/checkpoint juanpedromoreno/docker-spark /start-master.sh "$@"
+docker pull juanpedromoreno/docker-spark:2.11_1.4.1
+docker run -d -t -p 7077:7077 -p 8080:8080 -P --name spark_master juanpedromoreno/docker-spark:2.11_1.4.1 /start-master.sh "$@"
--- start-worker.sh
+++ start-worker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker pull juanpedromoreno/docker-spark
-docker run -d -t -P -v /Users/juanpedromoreno/ab/:/opt/conf -v /Users/juanpedromoreno/workspace/47deg/aurora-breeze/trunk/checkpoint:/usr/local/spark/checkpoint --link spark_master:spark_master juanpedromoreno/docker-spark /start-worker.sh "$@"
+docker pull juanpedromoreno/docker-spark:2.11_1.4.1
+docker run -d -t -P --link spark_master:spark_master juanpedromoreno/docker-spark:2.11_1.4.1 /start-worker.sh "$@"
